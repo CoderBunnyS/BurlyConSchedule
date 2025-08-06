5566f43651bca42643c34d0a80936cdb5132f1b3
Bunny <67618232+CoderBunnyS@users.noreply.github.com>
2025-08-06 14:31:15 -0700

    test: render App within router

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders BurlyCon home page', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  const headingElement = screen.getByText(/BurlyCon Sparkle Squad/i);
+  expect(headingElement).toBeInTheDocument();
 });
